78154914f94f916fca0670d08a4195980af53934
Rahul Kavi <1053406+aceveggie@users.noreply.github.com>
2025-07-23 07:36:13 -0700

    made the code generic

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,25 +6,30 @@ from langchain_openai import ChatOpenAI
 import time
 from langchain_ollama import ChatOllama
 
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
+
+def ice_break_with(name: str) -> str:
+    linkedin_username_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username_url)
+    summary_template = '''
+    given the linkedin information {information} about a person, I want you to create:
+    1. A short summary
+    2. two interesting facts about their career (not linkedin profile)
+    '''
+    summary_prompt_template = PromptTemplate(
+        input_variables=['information'],
+        template=summary_template,
+    )
+    llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={'information': linkedin_data})
+    print(res)
+
+
 # TASK: Given a text about a person, return the following: 1. Summary about the person 2. Two interesting facts.
 if __name__ == "__main__":
     load_dotenv()
-    print("Hello LangChain!")
-    # given information about a person
-    information = '''Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman known for his leadership of Tesla, SpaceX, and X (formerly Twitter). Since 2025, he has been a senior advisor to United States president Donald Trump and the de facto head of the Department of Government Efficiency (DOGE). Musk has been considered the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion. He was named Time magazine's Person of the Year in 2021. Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He graduated from the University of Pennsylvania in the U.S. before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became a U.S. citizen.'''
-
-    # describe summary_template
-    summary_template = '''Given this text about a person: {information}, I want you to return the following in less than 500 words: 1. One paragraph summary about the person (max 5 sentences) 2. Two interesting facts.'''
-
-    summary_prompt_template = PromptTemplate(input_variables=['information'], template=summary_template)
-    # llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo') # takes forever to run locally
-    llm = ChatOllama(model='llama2:latest') # reasonably fast 20 sec call for 1000 words
-    chain = summary_prompt_template | llm
-    # chain = summary_prompt_template | llm | StrOutputParser # this is taking really long and 
-    start_time = time.time()
-    print('started calling')
-    res = chain.invoke(input={'information': information})
-    print('finished calling')
-    print('time taken', time.time()-start_time)
-    # print(res)
-    print(res.content)
+    print('Ice Breaker enter')
+    ice_break_with(name='Rahul Kavi ServiceNow')
